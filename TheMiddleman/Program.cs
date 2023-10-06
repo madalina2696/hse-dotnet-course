9032e9f60872321c7ed0b720782cb76d7f22b45e
Madalina-Iulia Militaru <77458882+madalina2696@users.noreply.github.com>
2023-10-06 18:21:16 +0000

    iteration 1

---
+++ TheMiddleman/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
+class Application
+{
+    static void Main()
+    {
+        var intermediaries = new List<Intermediary>();
+        var currentDay = 1;
+
+        Console.WriteLine("Wieviel Zwischenhändler nehmen teil? ");
+
+        int.TryParse(Console.ReadLine(), out int intermediaryCount);
+
+        for (int index = 1; index <= intermediaryCount; index++)
+        {
+            Console.WriteLine($"Name von Zwischenhändler {index}: ");
+            var individualName = Console.ReadLine() ?? "";
+
+            Console.WriteLine($"Name der Firma von {individualName}: ");
+            var organizationName = Console.ReadLine() ?? "";
+
+            intermediaries.Add(new Intermediary { Name = individualName, Company = organizationName });
+        }
+
+        while (true)
+        {
+            foreach (var intermediary in intermediaries)
+            {
+                Console.WriteLine($"{intermediary.Name} von {intermediary.Company} | Tag {currentDay}");
+                Console.WriteLine("b) Runde beenden");
+                Console.WriteLine("q) Programm schiessen");
+
+                var selectedOption = Console.ReadLine() ?? "q";
+
+                if (selectedOption.Equals("b", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+                else if (selectedOption.Equals("q", StringComparison.OrdinalIgnoreCase) || selectedOption == "")
+                {
+                    return;
+                }
+            }
+
+            currentDay++;
+        }
+    }
+
+    private class Intermediary
+    {
+        public string? Name { get; set; }
+        public string? Company { get; set; }
+    }
+}
